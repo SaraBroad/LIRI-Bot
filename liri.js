956e5b75cc79fee53b3b80247b76ac3b5eae0925
Sara Broad <sarabroad@Saras-MacBook-Pro-2.local>
2018-04-09 08:58:58 -0400

    First commit

--- liri.js
+++ liri.js
@@ -52,8 +52,19 @@ function searchTweets() {
 
 
 function searchSpotify(songName) {
-    
-
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        } else if (songName) {
+      //if song name is chosen 
+      console.log("artist");
+      console.log("song name");
+      console.log("link preview");
+      console.log("album") 
+        } else {
+            //song name is "The Sign" by Ace of Base
+        }
+      });
 }
 
 function searchFlicks(movieName) {
@@ -62,28 +73,6 @@ function searchFlicks(movieName) {
 
 main(process.argv[2], process.argv[3]);
 
-
-
-// 2. `node liri.js spotify-this-song '<song name here>'`
-
-//    * This will show the following information about the song in your terminal/bash window
-//      * Artist(s)
-//      * The song's name
-//      * A preview link of the song from Spotify
-//      * The album that the song is from
-//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
-   
-//    * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
-   
-//    * Like the Twitter API, the Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a **client id** and **client secret**:
-
-//    * Step One: Visit <https://developer.spotify.com/my-applications/#!/>
-   
-//    * Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-
-//    * Step Three: Once logged in, navigate to <https://developer.spotify.com/my-applications/#!/applications/create> to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-
-//    * Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the [node-spotify-api package](https://www.npmjs.com/package/node-spotify-api).
 
 // 3. `node liri.js movie-this '<movie name here>'`
 
